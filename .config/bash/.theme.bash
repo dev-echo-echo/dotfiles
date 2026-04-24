b09648cfaa65d178da1042be654cf1cc0727ebd1
echo00515 <one.one.just.accept.it@gmail.com>
2026-04-24 12:53:23 +0100

    modified .theme.bash

--- .config/bash/.theme.bash
+++ .config/bash/.theme.bash
@@ -1,17 +1,1 @@
-source .ombre.sh
-source .colors.sh
-
-function prompt_command() {
-    user_name=$(whoami)
-    ombre "$user_name" 
-    user="$gradient"
-    current_dir="${PWD/#"$HOME"/"~"}"
-    ombre "$current_dir"
-    dir="$gradient"
-    current_git_branch=$(git branch --show-current 2>/dev/null) 
-    git=$(if [[ -n "$current_git_branch" ]]; then echo " [ $current_git_branch]"; fi )
-	env=$(if [[ -n "$VIRTUAL_ENV" ]]; then echo " ($(basename $VIRTUAL_ENV)🐍)"; fi)
-	PS1="\n╭─\[$user\]\[$PINK\]$git\[$RESET$ORANGE\]$env\[$RESET \[$dir\] \n╰──> "
-}
-
-PROMPT_COMMAND=prompt_command 
+.theme.bash
